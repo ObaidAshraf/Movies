0625aab97d125be8dc1394d46ac19a07e1d26e6e
obaidashraf <ubaidashraf91@gmail.com>
2023-11-16 16:06:34 +0500

    Added admin.py and migrations. Also fetched data from Movie model and sent to movies.html

--- movies/views.py
+++ movies/views.py
@@ -1,28 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-data = {
-    'movies': [
-        {
-            'id': 5,
-            'title': 'Movie1',
-            'year': 2000
-        },
-        {
-            'id': 6,
-            'title': 'Movie2',
-            'year': 2010
-        },
-        {
-            'id': 7,
-            'title': 'Movie3',
-            'year': 2020
-        },
-    ]
-}
+from .models import Movie
 
 def movies(request):
-    return render(request, 'movies/movies.html', data)
+    data = Movie.objects.all()
+    return render(request, 'movies/movies.html', {'movies': data})
 
 def home(request):
     return HttpResponse("Home Page")
