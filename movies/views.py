cc6dd985f333de5e7aa30857799dfd82168ef136
obaidashraf <ubaidashraf91@gmail.com>
2023-11-19 23:53:45 +0500

    minor modification

--- movies/views.py
+++ movies/views.py
@@ -38,7 +38,7 @@ class SingleMovieDetailView(TemplateView):
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        movie = get_object_or_404(Movie, pk = self.kwargs.get('pk'))
+        movie = get_object_or_404(Movie, pk = kwargs.get('pk'))
         context['movie'] = movie
         return context
 
