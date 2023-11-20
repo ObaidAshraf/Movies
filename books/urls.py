e866b985880ae7e0b93e5953c54a257ae2f430c4
obaidashraf <ubaidashraf91@gmail.com>
2023-11-21 00:05:14 +0500

    Added DetailView

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,8 @@
 
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, BookDetailView
 
 urlpatterns = [
     path('', IndexView.as_view(), name = 'index'),
-    # path('<slug:slug>/', BookDetailView.as_view(), name = 'book-detail')
+    path('<slug:slug>/', BookDetailView.as_view(), name = 'book-detail')
 ]
--- books/views.py
+++ books/views.py
@@ -1,6 +1,8 @@
 from typing import Any
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
+from django.utils import timezone
 from .models import Book
 
 class IndexView(TemplateView):
@@ -10,5 +12,18 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context['books'] = Book.objects.all()
+        return context
+    
+
+class BookDetailView(DetailView):
+
+    model = Book
+    template_name = "books/book_detail.html"
+    context_object_name = "book"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+
+        context['time'] = timezone.now()
         return context
 
